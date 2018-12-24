16cc23095b66067b5dbe9c5a4c1a280c731f949e
BZIvanov <ivanov.biser@mail.bg>
2018-12-24 11:41:54 +0200

    update

---
+++ 01.ExpressJS/01.IntroToHttpAndNode/Tasks/handlers/index.js
@@ -0,0 +1,6 @@
+// we will keep all handlers here so we can export all them as an array, not 1 by 1
+const homeHandler = require('./home');
+const filesHandler = require('./static-files');
+const productHandler = require('./product');
+
+module.exports = [ homeHandler, filesHandler, productHandler ];
