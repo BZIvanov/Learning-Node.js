50ca45180ff7897f2397a40d4aa29528eca22330
BZIvanov <ivanov.biser@mail.bg>
2018-12-25 12:42:41 +0200

    update

--- 01.ExpressJS/01.IntroToHttpAndNode/Tasks/config/test.js
+++
@@ -1,9 +0,0 @@
-let pros = require('./database')
-
-console.log(pros.products.add("Kifla"))
-console.log(pros.products.add("Kroasan"))
-console.log(pros.products.add("Marcipan"))
-
-console.log(pros.products.getAll())
-
-console.log(pros.products.findByName('Kifl'))
