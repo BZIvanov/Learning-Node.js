f9a068cd5ef575b09e147b8df933517f5dcc6c2a
BZIvanov <ivanov.biser@mail.bg>
2018-12-28 19:34:04 +0200

    update

---
+++ 01.ExpressJS/04.IntroToExpress/Tasks/MemeDBwithExpress/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fileUploader = require('express-fileupload');
+
+const homeModule = require('./modules/homeModule');
+const memeModule = require('./modules/memeModule');
+const apiModule = require('./modules/apiModule');
+
+const port = 3000;
+
+const app = express();
+
+app.use('/public',express.static('./public'));
+app.use(fileUploader());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.use('/',homeModule);
+app.use('/memes',memeModule);
+app.use('/api',apiModule);
+
+require('./config/dbConfig')
+	.then(() => {
+		app.listen(port,() => console.log('Im listening on ' + port));
+	})
+	.catch(() => {
+		console.log('Failed to load DB');
+	});
