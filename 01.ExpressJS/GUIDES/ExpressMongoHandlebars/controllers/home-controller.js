c9acc5f7a672bc70dbf538c868b339835ef603e7
BZIvanov <ivanov.biser@mail.bg>
2018-12-28 19:34:04 +0200

    update

--- 01.ExpressJS/GUIDES/ExpressMongoHandlebars/controllers/home-controller.js
+++
@@ -1,9 +0,0 @@
-const Book = require('../data/book');
-
-module.exports = {
-    getIndex: (req, res) => {
-        Book.count().then(books => {
-            res.render('index', { books })
-        })
-    }
-}
