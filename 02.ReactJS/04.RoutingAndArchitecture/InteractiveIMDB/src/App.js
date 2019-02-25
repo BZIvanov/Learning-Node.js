4a8ef56bb734a1a45dedab5c3d50f84d18eb4582
BZIvanov <ivanov.biser@mail.bg>
2019-02-25 08:16:08 +0200

    update

---
+++ 02.ReactJS/04.RoutingAndArchitecture/InteractiveIMDB/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+import Home from './Home/Home';
+import Register from './Register/Register';
+import Login from './Login/Login';
+import Create from './Create/Create';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+         { /* TODO */ }
+      </div>
+    );
+  }
+}
+
+export default App;
