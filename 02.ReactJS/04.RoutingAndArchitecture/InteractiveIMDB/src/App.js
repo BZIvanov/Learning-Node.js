04e16043dd228d7e2ff14779739a92ca7de8d2f6
BZIvanov <ivanov.biser@mail.bg>
2019-02-25 17:57:42 +0200

    update

--- 02.ReactJS/04.RoutingAndArchitecture/InteractiveIMDB/src/App.js
+++ 02.ReactJS/04.RoutingAndArchitecture/InteractiveIMDB/src/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 
 import Home from './Home/Home';
 import Register from './Register/Register';
 import Login from './Login/Login';
 import Create from './Create/Create';
 import './App.css';
+import Header from './Header/Header';
 
 class App extends Component {
+  handleChange() {
+    
+  }
   render() {
     return (
       <div className="App">
-         { /* TODO */ }
+         <Header />
+         <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
+          <Route render={() => <h1>Not found!</h1>} />
+         </Switch>
       </div>
     );
   }
