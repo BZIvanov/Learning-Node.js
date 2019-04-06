f1906f2da50c573e808bf211cf9090f59094b3df
BZIvanov <ivanov.biser@mail.bg>
2019-04-06 08:39:58 +0300

    update

--- 03.Angular/07.DirectivesAndForms/form-app/src/app/app.module.ts
+++ 03.Angular/07.DirectivesAndForms/form-app/src/app/app.module.ts
@@ -4,17 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterFormComponent } from './register-form/register-form.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ImageUrlValidateDirective } from './image-url-validate.directive';
+import { RegisterFormReactiveComponent } from './register-form-reactive/register-form-reactive.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterFormComponent
+    RegisterFormComponent,
+    ImageUrlValidateDirective,
+    RegisterFormReactiveComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
