a5e2f3f285d3273b3af73c87eae5d30d7262a79b
BZIvanov <ivanov.biser@gmx.com>
2019-09-22 12:55:26 +0300

    update

--- 02.ReactJS/97.Dashboard/server/routes/feed.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const feedController = require('../controllers/feed');
-
-router.post('/brand/create', feedController.createBrand);
-router.get('/brand/all', feedController.getBrands);
-
-module.exports = router;
