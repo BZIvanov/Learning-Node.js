92762c5c2cca3d938cee327447daf8761bc0aab2
BZIvanov <biser.z.ivanov@gmail.com>
2020-01-24 16:34:35 +0200

    removed useless info files

--- 02.ReactJS/02.Components/Demo/src/lectures/lecture-2/books-list.jsx
+++ 02.ReactJS/02.Components/Demo/src/lectures/lecture-2/books-list.jsx
@@ -1,34 +1,32 @@
 const books = [
-    {
-        bookId: 1,
-        title: 'Lord of the Rings',
-        author: 'J.R.R. Tolkien',
-    },
-    {
-        bookId: 2,
-        title: 'Harry Potter and the Order of the Phoenix',
-        author: 'J.K. Rowling',
-    },
-    {
-        bookId: 3,
-        title: 'Game of Throne',
-        author: 'J.R.R. Martyn',
-    },
+  {
+    bookId: 1,
+    title: 'Lord of the Rings',
+    author: 'J.R.R. Tolkien',
+  },
+  {
+    bookId: 2,
+    title: 'Harry Potter and the Order of the Phoenix',
+    author: 'J.K. Rowling',
+  },
+  {
+    bookId: 3,
+    title: 'Game of Throne',
+    author: 'J.R.R. Martyn',
+  },
 ];
 
 function BookList(props) {
-    const { books } = props;
+  const { books } = props;
 
-    return (
-        <ul>
-            {
-                books.map(book => (
-                    <li>
-                        <h5>Title: {book.title}</h5>
-                        <h6>Author: {book.author}</h6>
-                    </li>  
-                ))
-            }
-        </ul>
-    );
+  return (
+    <ul>
+      {books.map(book => (
+          <li>
+            <h5>Title: {book.title}</h5>
+            <h6>Author: {book.author}</h6>
+          </li>  
+        ))}
+    </ul>
+  );
 }
