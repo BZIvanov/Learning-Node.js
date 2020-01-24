ef46ab14267284c4b94b6c24b35b791b4e6d5840
BZIvanov <ivanov.biser@gmx.com>
2020-01-24 16:34:35 +0200

    removed useless info files

--- 02.ReactJS/02.Components/Demo/src/lectures/lecture-1/index.js
+++ 02.ReactJS/02.Components/Demo/src/lectures/lecture-1/index.js
@@ -1,57 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 
 function renderApp() {
-    ReactDOM.render(
-        <Header />,
-        document.getElementById('root')
-    );
+  ReactDOM.render(<Header />, document.getElementById('root'));
 }
 
 let logoText = 'Logo placeholder';
 
 const handleNavLinkClick = (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const content = event.target.innerHTML;
+  const content = event.target.innerHTML;
 
-    logoText = content;
-    renderApp();
+  logoText = content;
+  renderApp();
 };
 
 const Logo = () => {
-    return (
-        <div>
-            <h1>{logoText}</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>{logoText}</h1>
+    </div>
+  );
 };
 
 const Navigation = () => {
-    const className = 'site-nav';
-    const linkClassName = `${className}-link`;
+  const className = 'site-nav';
+  const linkClassName = `${className}-link`;
 
-    return (
-        <nav className={className}>
-            <ul>
-                <li><a className={linkClassName} href="/" onClick={handleNavLinkClick}>Home</a></li>
-                <li><a className={linkClassName} href="/contact" onClick={handleNavLinkClick}>Contact us</a></li>
-                <li><a className={linkClassName} href="/about" onClick={handleNavLinkClick}>About</a></li>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className={className}>
+      <ul>
+        <li><a className={linkClassName} href="/" onClick={handleNavLinkClick}>Home</a></li>
+        <li><a className={linkClassName} href="/contact" onClick={handleNavLinkClick}>Contact us</a></li>
+        <li><a className={linkClassName} href="/about" onClick={handleNavLinkClick}>About</a></li>
+      </ul>
+    </nav>
+  );
 };
 
 const Header = () => {
-    return (
-        <header>
-            <Logo />
-            <Navigation />
-        </header>
-    );
+  return (
+    <header>
+      <Logo />
+      <Navigation />
+    </header>
+  );
 };
-
 
 renderApp();
