55d5cd35921ac37c40f62339ec4144fd480794d9
BZIvanov <ivanov.biser@gmx.com>
2020-01-26 20:30:55 +0200

    remove not used info files

--- 01.ExpressJS/GUIDES/Demos/ExcludingExpress/08.Demo-Formidable_module/index.js
+++ 01.ExpressJS/GUIDES/Demos/ExcludingExpress/08.Demo-Formidable_module/index.js
@@ -4,52 +4,52 @@ const formidable = require('formidable');
 const shortid = require('shortid');
 
 http.createServer((req, res) => {
-    if(req.method === 'GET') {
-        fs.readFile('./index.html', 'utf8', (err, data) => {
-            if(err) {
-                console.log(err);
-                return;
-            }
-    
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            });
-            res.write(data);
-            res.end();
-        });
-    } else if(req.method === 'POST') {
-        // here with shortid module we will generate some random name for the new file. This will also help to overwrite files with equal names
-        let randomName = shortid.generate();
+  if(req.method === 'GET') {
+    fs.readFile('./index.html', 'utf8', (err, data) => {
+      if(err) {
+        console.log(err);
+        return;
+      }
 
-        let form = new formidable.IncomingForm();
+      res.writeHead(200, {
+        'Content-Type': 'text/html'
+      });
+      res.write(data);
+      res.end();
+    });
+  } else if(req.method === 'POST') {
+    // here with shortid module we will generate some random name for the new file. This will also help to overwrite files with equal names
+    let randomName = shortid.generate();
 
-        // we will first check for event error, if there is any we return
-        form.on('error', (err) => {
-            console.log(err);
-            return
-        }).on('fileBegin', (name, file) => {
-            // with fileBegin event we can save the image whereever we want before it start downloading in default folder
-            file.path = `./files/${randomName}.jpg`;
-        });
+    let form = new formidable.IncomingForm();
 
-        // in the callback we receive error as first parameter, data from input fields and files from input fields
-        form.parse(req, (err, fields, files) => {
-            if(err) {
-                console.log(err);
-            }
+    // we will first check for event error, if there is any we return
+    form.on('error', (err) => {
+      console.log(err);
+      return
+    }).on('fileBegin', (name, file) => {
+      // with fileBegin event we can save the image whereever we want before it start downloading in default folder
+      file.path = `./files/${randomName}.jpg`;
+    });
 
-            //console.log(fields);
-            // fields is object where the keys are like the names from the form inputs and values are from form input values
-            let myName = fields.firstName;
-            let myPass = fields.password;
-            console.log(myName, myPass);
-            
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            });
-            res.write('<h1>Thank you</h1>');
-            res.end();
-        })
-    }
+    // in the callback we receive error as first parameter, data from input fields and files from input fields
+    form.parse(req, (err, fields, files) => {
+      if(err) {
+        console.log(err);
+      }
+
+      //console.log(fields);
+      // fields is object where the keys are like the names from the form inputs and values are from form input values
+      let myName = fields.firstName;
+      let myPass = fields.password;
+      console.log(myName, myPass);
+      
+      res.writeHead(200, {
+        'Content-Type': 'text/html'
+      });
+      res.write('<h1>Thank you</h1>');
+      res.end();
+    })
+  }
     
 }).listen(3000, () => console.log(`Listening on port ${3000}...`));
--- 02.ReactJS/05.HigherOrderComponents/Demo/src/components/BooksList.jsx
+++ 02.ReactJS/05.HigherOrderComponents/Demo/src/components/BooksList.jsx
@@ -3,29 +3,25 @@ import BooksService from '../services/books-service';
 import DataFromServiceProvider from './render-with-data-from-service.jsx';
 
 function BookList({ data, theme }) {
-    if (!data.length) {
-        return null;
-    }
+  if (!data.length) {
+    return null;
+  }
 
-    return (
-        <ul className={theme}>
-            {
-                data.map(book => (
-                    <li key={book.id}>
-                        Title: {book.title}
-                    </li>
-                ))
-            }
-        </ul>
-    );
+  return (
+    <ul className={theme}>
+      {data.map(book => (
+        <li key={book.id}>Title: {book.title}</li>
+      ))}
+    </ul>
+  );
 }
 
 const BookListWithServiceDataProvider = ({ theme }) => (
-    <DataFromServiceProvider
-        initialData={[]}
-        serviceMethod={new BooksService().getBooks}
-        render={(data) => <BookList data={data} theme={theme} />}
-    />
+  <DataFromServiceProvider
+    initialData={[]}
+    serviceMethod={new BooksService().getBooks}
+    render={(data) => <BookList data={data} theme={theme} />}
+  />
 );
 
 export default BookListWithServiceDataProvider;
