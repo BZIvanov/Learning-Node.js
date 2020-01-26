28e5b33e1d1f11fb93b93e76bdb4f31005046f8f
BZIvanov <ivanov.biser@gmx.com>
2020-01-26 19:10:21 +0200

    remove duplicated and not used info files

--- 03.Angular/01.IntroToAngular/04.elemelons.ts
+++ 03.Angular/01.IntroToAngular/04.elemelons.ts
@@ -1,93 +1,93 @@
 abstract class Melon {
-    public weight: number;
-    public melonSort: string;
+  public weight: number;
+  public melonSort: string;
 
-    constructor(weight: number, melonSort: string) {
-        this.weight = weight;
-        this.melonSort = melonSort;
-    }
+  constructor(weight: number, melonSort: string) {
+    this.weight = weight;
+    this.melonSort = melonSort;
+  }
 }
 
 class Watermelon extends Melon {
-    public element: string = "Water";
+  public element: string = "Water";
 
-    constructor(weight: number, melonSort: string) {
-        super(weight, melonSort);
-    }
+  constructor(weight: number, melonSort: string) {
+    super(weight, melonSort);
+  }
 
-    get elementIndex(): number {
-        return this.weight * this.melonSort.length;
-    }
+  get elementIndex(): number {
+    return this.weight * this.melonSort.length;
+  }
 
-    toString(): string {
-        // elementIndex is getter and we call it as object property not as function
-        return `Element: ${this.element}\nSort: ${this.melonSort}\nElement Index: ${this.elementIndex}`
-    }
+  toString(): string {
+    // elementIndex is getter and we call it as object property not as function
+    return `Element: ${this.element}\nSort: ${this.melonSort}\nElement Index: ${this.elementIndex}`
+  }
 }
 
 class Firemelon extends Melon {
-    public element: string = "Fire";
+  public element: string = "Fire";
 
-    constructor(weight: number, melonSort: string) {
-        super(weight, melonSort);
-    }
+  constructor(weight: number, melonSort: string) {
+    super(weight, melonSort);
+  }
 
-    get elementIndex(): number {
-        return this.weight * this.melonSort.length;
-    }
+  get elementIndex(): number {
+    return this.weight * this.melonSort.length;
+  }
 
-    toString(): string {
-        return `Element: ${this.element}\nSort: ${this.melonSort}\nElement Index: ${this.elementIndex}`
-    }
+  toString(): string {
+    return `Element: ${this.element}\nSort: ${this.melonSort}\nElement Index: ${this.elementIndex}`
+  }
 }
 
 class Earthmelon extends Melon {
-    public element: string = "Earth";
+  public element: string = "Earth";
 
-    constructor(weight: number, melonSort: string) {
-        super(weight, melonSort);
-    }
+  constructor(weight: number, melonSort: string) {
+    super(weight, melonSort);
+  }
 
-    get elementIndex(): number {
-        return this.weight * this.melonSort.length;
-    }
+  get elementIndex(): number {
+    return this.weight * this.melonSort.length;
+  }
 
-    toString(): string {
-        return `Element: ${this.element}\nSort: ${this.melonSort}\nElement Index: ${this.elementIndex}`
-    }
+  toString(): string {
+    return `Element: ${this.element}\nSort: ${this.melonSort}\nElement Index: ${this.elementIndex}`
+  }
 }
 
 class Airmelon extends Melon {
-    public element: string = "Air";
+  public element: string = "Air";
 
-    constructor(weight: number, melonSort: string) {
-        super(weight, melonSort);
-    }
+  constructor(weight: number, melonSort: string) {
+    super(weight, melonSort);
+  }
 
-    get elementIndex(): number {
-        return this.weight * this.melonSort.length;
-    }
+  get elementIndex(): number {
+    return this.weight * this.melonSort.length;
+  }
 
-    toString(): string {
-        return `Element: ${this.element}\nSort: ${this.melonSort}\nElement Index: ${this.elementIndex}`
-    }
+  toString(): string {
+    return `Element: ${this.element}\nSort: ${this.melonSort}\nElement Index: ${this.elementIndex}`
+  }
 }
 
 class Melolemonmelon extends Watermelon {
-    public _elements: string[];
-    public element: string;
+  public _elements: string[];
+  public element: string;
 
-    constructor(weight: number, melonSort: string) {
-        super(weight, melonSort);
-        this._elements = ['Water', 'Fire', 'Earth', 'Air'];
-        this.morph();
-    }
+  constructor(weight: number, melonSort: string) {
+    super(weight, melonSort);
+    this._elements = ['Water', 'Fire', 'Earth', 'Air'];
+    this.morph();
+  }
 
-    morph(): void {
-        let current = this._elements.shift();
-        this.element = current;
-        this._elements.push(current);
-    }
+  morph(): void {
+    let current = this._elements.shift();
+    this.element = current;
+    this._elements.push(current);
+  }
 }
 
 let watermelon: Watermelon = new Watermelon(12.5, "Kingsize");
--- 03.Angular/01.IntroToAngular/06.keyValuePairs.ts
+++ 03.Angular/01.IntroToAngular/06.keyValuePairs.ts
@@ -1,15 +1,15 @@
 class KeyValuePair<T, U> {
-    private key: T;
-    private val: U;
+  private key: T;
+  private val: U;
 
-    setKeyValue(key: T, val: U) {
-        this.key = key;
-        this.val = val;
-    }
+  setKeyValue(key: T, val: U) {
+    this.key = key;
+    this.val = val;
+  }
 
-    display(): void {
-        console.log(`key = ${this.key}, value = ${this.val}`);
-    }
+  display(): void {
+    console.log(`key = ${this.key}, value = ${this.val}`);
+  }
 }
 
 let kvp = new KeyValuePair<number, string>();
