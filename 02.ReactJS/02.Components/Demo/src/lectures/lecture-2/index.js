b8bb583feae8ef25d7dfea031df99e95d8b8308d
BZIvanov <biser.z.ivanov@gmail.com>
2020-02-01 20:02:51 +0200

    code improvement

--- 02.ReactJS/02.Components/Demo/src/lectures/lecture-2/index.js
+++ 02.ReactJS/02.Components/Demo/src/lectures/lecture-2/index.js
@@ -1,73 +1,64 @@
 function Nav(props) {
-    const {className, children, replaceAll} = props;
-    const navItems = replaceAll
-        ? (children)
-        : [
-            <a href="/">Home</a>,
-            children
-        ];
+  const {className, children, replaceAll} = props;
+  const navItems = replaceAll
+    ? (children)
+    : [<a href="/">Home</a>, children];
 
-    return (
-        <nav className={className}>
-            {navItems}
-        </nav>
-    );
+  return (
+    <nav className={className}>
+      {navItems}
+    </nav>
+  );
 }
 
 function Footer(props) {
-    const footerClass = 'footer';
-    const footerNavClass = `${footerClass}-navigation`;
+  const footerClass = 'footer';
+  const footerNavClass = `${footerClass}-navigation`;
 
-    return (
-        <footer>
-            <Nav className={footerNavClass} />
-        </footer>
-    );
+  return (
+    <footer>
+      <Nav className={footerNavClass} />
+    </footer>
+  );
 }
 
 function Header(props) {
-    const headerClass = 'header';
-    const headerNavClass = `${headerClass}-navigation`;
+  const headerClass = 'header';
+  const headerNavClass = `${headerClass}-navigation`;
 
-    return (
-        <header>
-            <Nav className={headerNavClass} replaceAll={true}>
-                <a href="#">About</a>
-                <a href="#">Contact</a>
-            </Nav>
-        </header>
-    );
+  return (
+    <header>
+      <Nav className={headerNavClass} replaceAll={true}>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+      </Nav>
+    </header>
+  );
 }
 
 function HeaderAndFooter() {
-    return (
-        <Fragment>
-            <Header />
-            <Footer />
-        </Fragment>
-    );
+  return (
+    <Fragment>
+      <Header />
+      <Footer />
+    </Fragment>
+  );
 }
 
 function NavWithString(props) {
-    const { items } = props;
+  const { items } = props;
 
-    if (!items) {
-        return null;
-    }
+  if (!items) {
+    return null;
+  }
 
-    return (
-        <nav>
-            <ul>
-                {
-                    items.map(item => (
-                        <li>
-                            <a href="#">{item}</a>
-                        </li>
-                    ))
-                }
-            </ul>
-        </nav>
-    );
+  return (
+    <nav>
+      <ul>
+        {items.map(item => <li><a href="#">{item}</a></li>)}
+      </ul>
+    </nav>
+  );
 }
 
 ReactDOM.render(
