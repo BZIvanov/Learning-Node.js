d77839f3ee147f6055049bb65382f5b6bfe66eaf
BZIvanov <biser.z.ivanov@gmail.com>
2020-05-01 06:49:36 +0300

    removed duplicate code

--- 01.ExpressJS/99.Exams/Messenger/config/routes.js
+++ 01.ExpressJS/99.Exams/Messenger/config/routes.js
@@ -1,21 +1,25 @@
 const controllers = require('../controllers');
 const restrictedPages = require('./auth');
 
-module.exports = app => {
-    // here we dont call functions just provide them
-    app.get('/', controllers.home.index);
-    app.get('/users/register', controllers.user.registerGet);
-    app.post('/users/register', controllers.user.registerPost);
-    app.get('/users/logout', controllers.user.logout);
-    app.get('/users/login', controllers.user.loginGet);
-    app.post('/users/login', controllers.user.loginPost);
+module.exports = (app) => {
+  // here we dont call functions just provide them
+  app.get('/', controllers.home.index);
+  app.get('/users/register', controllers.user.registerGet);
+  app.post('/users/register', controllers.user.registerPost);
+  app.get('/users/logout', controllers.user.logout);
+  app.get('/users/login', controllers.user.loginGet);
+  app.post('/users/login', controllers.user.loginPost);
 
-    app.post('/threads/find', controllers.threads.findUser);
-    app.post('/message/send', controllers.threads.sendMessage);
+  app.post('/threads/find', controllers.threads.findUser);
+  app.post(
+    '/message/send',
+    restrictedPages.hasRole('User'),
+    controllers.threads.sendMessage
+  );
 
-    app.all('*', (req, res) => {
-        res.status(404);
-        res.send('404 Not Found');
-        res.end();
-    });
+  app.all('*', (req, res) => {
+    res.status(404);
+    res.send('404 Not Found');
+    res.end();
+  });
 };
