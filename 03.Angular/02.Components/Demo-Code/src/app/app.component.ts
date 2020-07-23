0ca21f741b30cf2e3ffa11b768d0b93221a814d0
BZIvanov <biser.z.ivanov@gmail.com>
2020-07-23 23:23:39 +0300

    removed unused files

--- 03.Angular/02.Components/Demo-Code/src/app/app.component.ts
+++ 03.Angular/02.Components/Demo-Code/src/app/app.component.ts
@@ -1,19 +1,14 @@
-import { Component, ElementRef, ViewChild, ContentChild, AfterViewInit } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'softuni-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements AfterViewInit {
-
   @ViewChild('nameInput') nameInput: ElementRef;
 
   todos = [];
-
-  constructor() {
-
-  }
 
   ngAfterViewInit() {
     this.nameInput.nativeElement.value = 100;
