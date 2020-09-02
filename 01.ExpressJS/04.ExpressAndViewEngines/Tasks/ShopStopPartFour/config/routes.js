b0e0213486ae5ea365efce9f0314939ff802229d
BZIvanov <biser.z.ivanov@gmail.com>
2020-09-02 23:36:47 +0300

    replace let with const

--- 01.ExpressJS/04.ExpressAndViewEngines/Tasks/ShopStopPartFour/config/routes.js
+++ 01.ExpressJS/04.ExpressAndViewEngines/Tasks/ShopStopPartFour/config/routes.js
@@ -1,24 +1,28 @@
 const handlers = require('../handlers');
 const multer = require('multer');
 
-let upload = multer({dest: './content/images'});
+const upload = multer({ dest: './content/images' });
 
 module.exports = (app) => {
-    app.get('/', handlers.home.index);
+  app.get('/', handlers.home.index);
 
-    app.get('/product/add', handlers.product.addGet);
-    app.post('/product/add', upload.single('image'), handlers.product.addPost);
+  app.get('/product/add', handlers.product.addGet);
+  app.post('/product/add', upload.single('image'), handlers.product.addPost);
 
-    app.get('/category/add', handlers.category.addGet);
-    app.post('/category/add', handlers.category.addPost);
+  app.get('/category/add', handlers.category.addGet);
+  app.post('/category/add', handlers.category.addPost);
 
-    app.get('/category/:category/products', handlers.category.productByCategory);
+  app.get('/category/:category/products', handlers.category.productByCategory);
 
-    app.get('/product/delete/:id', handlers.product.deleteGet);
-	app.post('/product/delete/:id', handlers.product.deletePost);
+  app.get('/product/delete/:id', handlers.product.deleteGet);
+  app.post('/product/delete/:id', handlers.product.deletePost);
 
-    app.get('/product/edit/:id', handlers.product.editGet);
-    app.post('/product/edit/:id', upload.single('image'), handlers.product.editPost);
+  app.get('/product/edit/:id', handlers.product.editGet);
+  app.post(
+    '/product/edit/:id',
+    upload.single('image'),
+    handlers.product.editPost
+  );
 
-    app.get('/product/buy/:id', handlers.product.buyGet);
-}
+  app.get('/product/buy/:id', handlers.product.buyGet);
+};
--- 01.ExpressJS/04.ExpressAndViewEngines/Tasks/ShopStopPartFour/index.js
+++ 01.ExpressJS/04.ExpressAndViewEngines/Tasks/ShopStopPartFour/index.js
@@ -3,8 +3,8 @@ const port = 3000;
 const config = require('./config/config');
 const database = require('./config/database.config');
 
-let app = express();
-let environment = process.env.NODE_ENV || 'development';
+const app = express();
+const environment = process.env.NODE_ENV || 'development';
 
 database(config[environment]);
 require('./config/express')(app, config[environment]);
