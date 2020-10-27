99f6c487291e567f9fecee00338e15fd5f2fe513
BZIvanov <biser.z.ivanov@gmail.com>
2020-10-27 22:59:06 +0200

    imrpoved file formatting

--- 01.ExpressJS/99.Exams/ProjectManager/models/Project.js
+++ 01.ExpressJS/99.Exams/ProjectManager/models/Project.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose');
 
 const projectSchema = new mongoose.Schema({
-    name: { type: mongoose.Schema.Types.String, required: [true, 'Please add name of the project'], unique: true },
-    description: [{ type: mongoose.Schema.Types.String, required: [true, 'Please add description of the project'], maxlength: 50 }],
-    team: { type: mongoose.Schema.Types.ObjectId, ref: 'Team' }
+  name: {
+    type: mongoose.Schema.Types.String,
+    required: [true, 'Please add name of the project'],
+    unique: true,
+  },
+  description: [
+    {
+      type: mongoose.Schema.Types.String,
+      required: [true, 'Please add description of the project'],
+      maxlength: 50,
+    },
+  ],
+  team: { type: mongoose.Schema.Types.ObjectId, ref: 'Team' },
 });
 
 const Project = mongoose.model('Project', projectSchema);
--- 01.ExpressJS/99.Exams/ProjectManager/models/Team.js
+++ 01.ExpressJS/99.Exams/ProjectManager/models/Team.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 
 const teamSchema = new mongoose.Schema({
-    name: { type: mongoose.Schema.Types.String, required: [true, 'Please add name of the team'], unique: true },
-    projects: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Project' }],
-    members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  name: {
+    type: mongoose.Schema.Types.String,
+    required: [true, 'Please add name of the team'],
+    unique: true,
+  },
+  projects: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Project' }],
+  members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
 });
 
 const Team = mongoose.model('Team', teamSchema);
