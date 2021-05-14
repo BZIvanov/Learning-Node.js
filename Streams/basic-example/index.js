99d8ae27053a823b7d01e4f44660d60e9321feeb
BZIvanov <biser.z.ivanov@gmail.com>
2021-05-14 17:35:54 +0300

    basic read and write stream

---
+++ Streams/basic-example/index.js
@@ -0,0 +1,15 @@
+// in the text.txt you can find instructions to debug the code to get overview of jow the stream is created
+const fs = require('fs');
+
+// with highWaterMark we will create smaller chunks, otherwise we would need a lot of text
+const readable = fs.createReadStream(__dirname + '/text.txt', {
+  encoding: 'utf8',
+  highWaterMark: 128, // default is 64kb 64 * 1024
+});
+
+const writeable = fs.createWriteStream(__dirname + '/output.txt');
+
+readable.on('data', (chunk) => {
+  console.log(chunk + 'THIS CHUNK READY');
+  writeable.write(chunk);
+});
