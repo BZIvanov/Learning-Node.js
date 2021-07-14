def8180a0e9294d691270083573eff26919bbfe4
BZIvanov <biser.z.ivanov@gmail.com>
2021-07-14 20:19:20 +0300

    comment for pre and post npm commands

--- NPM/pre-post_commands/index.js
+++ NPM/pre-post_commands/index.js
@@ -1,1 +1,4 @@
+// from the package.json file run my-command and you will see all 3 commands executed
+// npm is automatically executing same command names starting with pre/post respectively before and after the main command
+
 console.log(process.env.MY_VAR);
