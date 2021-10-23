d244a94027e7aa3d35b442db004565ef53f9bebe
BZIvanov <biser.z.ivanov@gmail.com>
2021-10-23 19:58:33 +0300

    shop stop two path refactor

--- 01.ExpressJS/02.WebServer/Tasks/ShopStopPartTwo/index.js
+++ 01.ExpressJS/02.WebServer/Tasks/ShopStopPartTwo/index.js
@@ -2,7 +2,7 @@ const http = require('http');
 // in handlers folder we have many files, but by default if we have index file in the folder it will be loaded even if we dont specify its name
 const handlers = require('./handlers');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 http
   .createServer((req, res) => {
     for (const handler of handlers) {
