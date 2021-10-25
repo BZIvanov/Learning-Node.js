d3f771841bad82c69396f187da7f23dc6cccc542
BZIvanov <biser.z.ivanov@gmail.com>
2021-10-25 21:55:29 +0300

    shop stop 3 packages update

--- 01.ExpressJS/03.PersistenceAndMongoDB/Tasks/ShopStopPartThree/database/index.js
+++ 01.ExpressJS/03.PersistenceAndMongoDB/Tasks/ShopStopPartThree/database/index.js
@@ -1,11 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = () => {
-  mongoose.connect(process.env.DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  });
+  mongoose.connect(process.env.DB_URI, {});
 
   const database = mongoose.connection;
 
