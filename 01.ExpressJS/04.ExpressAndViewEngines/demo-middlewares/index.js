94cdb929a95b6d0937040bae7a0436cf845e5212
BZIvanov <ivanov.biser@gmx.com>
2022-09-16 22:46:18 +0300

    update middleware demo

--- 01.ExpressJS/04.ExpressAndViewEngines/demo-middlewares/index.js
+++ 01.ExpressJS/04.ExpressAndViewEngines/demo-middlewares/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const app = express();
 
 // Middleware functions have third argument next() except the last callback. We can build logic with middlewares because if something is not satisified we can call another callback with req, res
@@ -15,7 +16,7 @@ app.get(
   middleWareAsFunction,
   (req, res) => {
     console.log('All before me were false and I am the last to finish!');
-    res.send('The last middleware can not have next argument');
+    res.send('We got to this middleware and sent the response');
   }
 );
 
@@ -30,5 +31,5 @@ function middleWareAsFunction(req, res, next) {
   }
 }
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
