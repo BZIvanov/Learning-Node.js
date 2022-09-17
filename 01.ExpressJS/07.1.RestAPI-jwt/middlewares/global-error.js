581a062b9c3c8b4af099e50f66cdaec4e208fd2d
BZIvanov <biser.z.ivanov@gmail.com>
2022-09-17 19:22:40 +0300

    not found route

--- 01.ExpressJS/07.1.RestAPI-jwt/middlewares/global-error.js
+++ 01.ExpressJS/07.1.RestAPI-jwt/middlewares/global-error.js
@@ -1,7 +1,7 @@
 const status = require('http-status');
 
 module.exports = function (err, req, res, next) {
-  console.log(err);
+  console.log('Global Error Middleware', err);
   res
     .status(status.INTERNAL_SERVER_ERROR)
     .json({ success: false, message: 'Something went wrong' });
