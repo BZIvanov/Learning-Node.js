c09d5b42f85e926207e2ab4ffda0c2b308474de2
BZIvanov <ivanov.biser@gmx.com>
2022-09-18 18:04:07 +0300

    add helmet

--- 01.ExpressJS/07.1.RestAPI-jwt/startup/express.js
+++ 01.ExpressJS/07.1.RestAPI-jwt/startup/express.js
@@ -1,6 +1,7 @@
 // here the dotenv variables are also loaded, because of the unit tests
 require('dotenv').config();
 const express = require('express');
+const helmet = require('helmet');
 const cors = require('cors');
 const morgan = require('morgan');
 const routesV1 = require('../routes/v1');
@@ -9,6 +10,7 @@ const globalError = require('../middlewares/global-error');
 
 const app = express();
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(morgan('combined'));
