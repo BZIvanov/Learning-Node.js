f797318e89cc62ede3ce83b30b3ec63fb16610ee
BZIvanov <biser.z.ivanov@gmail.com>
2023-01-29 21:25:55 +0200

    improve clusters example

--- Clusters/example-a/server.js
+++ Clusters/example-a/server.js
@@ -1,3 +1,4 @@
+const os = require('os');
 const cluster = require('cluster');
 const express = require('express');
 
@@ -7,15 +8,18 @@ app.get('/', (req, res) => {
   res.send(`Current process ID: ${process.pid}`);
 });
 
+console.log('\n');
 console.log('Running server.js');
 
 if (cluster.isMaster) {
-  console.log('Master has been started');
+  console.log('- Master has been started');
 
-  cluster.fork();
-  cluster.fork();
+  const CPUS_COUNT = os.cpus().length;
+  for (let i = 0; i < CPUS_COUNT; i++) {
+    cluster.fork();
+  }
 } else {
-  console.log('Worker process started');
+  console.log('-- Worker process started');
 
-  app.listen(3000, () => console.log('App listening...'));
+  app.listen(3000, () => console.log('--- App listening...'));
 }
