7519884b1bb1fcc2124a3527a54e6b8c9c22508e
BZIvanov <biser.z.ivanov@gmail.com>
2025-07-13 09:20:57 +0300

    remove impractical demos

--- 01.ExpressJS/03.PersistenceAndMongoDB/Tasks/ShopStopPartThree/models/Category.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const categorySchema = Schema({
-  name: { type: String, required: true, unique: true },
-  // ref propert is reference to another Collection in the database
-  products: [{ type: Schema.Types.ObjectId, ref: 'Product' }],
-});
-
-module.exports = model('Category', categorySchema);
